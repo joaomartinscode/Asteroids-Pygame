f2d5ee3f3e9c7d81098aaa9d6a26783b6a5fabf3
joaomartinscode <jfbrancomartins@gmail.com>
2026-01-29 02:22:22 +0000

    Create the playerobject

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -13,6 +14,8 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         log_state()
 
@@ -21,6 +24,8 @@ def main():
                 return
 
         screen.fill("black")
+        player.draw(screen)
+
         pygame.display.flip()
 
         tick = clock.tick(60)
